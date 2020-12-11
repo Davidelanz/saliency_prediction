1ab15b867b601264bfef533082d9b6126c650269
Davide Lanza <lanza.davide.it@gmail.com>
2020-12-11 15:13:06 +0000

    Added ckpt download instructions

--- checkpoints/download_ckpt.sh
+++ checkpoints/download_ckpt.sh
@@ -1,13 +1,6 @@
-# Checkpoints folder
-
-To automatically download Deep Gaze 2 and ICF models: ``download_ckpt.sh``
-
-Manually:
-```
 wget https://github.com/Davidelanz/saliency_prediction/releases/download/v0.1-alpha/DeepGazeII.ckpt.data-00000-of-00001
 wget https://github.com/Davidelanz/saliency_prediction/releases/download/v0.1-alpha/DeepGazeII.ckpt.index
 wget https://github.com/Davidelanz/saliency_prediction/releases/download/v0.1-alpha/DeepGazeII.ckpt.meta
 wget https://github.com/Davidelanz/saliency_prediction/releases/download/v0.1-alpha/ICF.ckpt.data-00000-of-00001
 wget https://github.com/Davidelanz/saliency_prediction/releases/download/v0.1-alpha/ICF.ckpt.index
 wget https://github.com/Davidelanz/saliency_prediction/releases/download/v0.1-alpha/ICF.ckpt.meta
-```
